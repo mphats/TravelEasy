15d6e8afd032784424aa28295b21d6697402ff2d
mphats <mphatsos45@gmail.com>
2024-12-13 13:47:54 +0100

    done

--- lib/base/bottom_nav_bar.dart
+++ lib/base/bottom_nav_bar.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:ticket_app/controller/bottom_nav_controller.dart';
 import 'package:ticket_app/screens/profile/profile.dart';
 import 'package:ticket_app/screens/search/search_screen.dart';
 import 'package:ticket_app/screens/ticket/ticket_screen.dart';
 
 import '../screens/home/home_screen.dart';
 
-class BottomNavBar extends StatefulWidget {
-  const BottomNavBar({super.key});
+class BottomNavBar extends StatelessWidget {
+  BottomNavBar({super.key});
 
-  @override
-  State<BottomNavBar> createState() => _BottomNavBarState();
-}
+  final BottomNavController controller = Get.put(BottomNavController());
 
-class _BottomNavBarState extends State<BottomNavBar> {
   final appScreens = [
     const HomeScreen(),
     const SearchScreen(),
@@ -20,31 +20,26 @@ class _BottomNavBarState extends State<BottomNavBar> {
     const Profile()
   ];
 
-  //change index dynamically
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index){
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
 
-      body: appScreens[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        onTap: _onItemTapped,
-        selectedItemColor: Colors.blueGrey,
-          unselectedItemColor: const Color(0xFF526400),
-          showSelectedLabels: false,
-          items: const [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-            BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
-            BottomNavigationBarItem(icon: Icon(Icons.airplane_ticket), label: "Tickets"),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
-          ]),
-    );
+    return Obx((){
+      return Scaffold(
+
+        body: appScreens[controller.selectedIndex.value],
+        bottomNavigationBar: BottomNavigationBar(
+            currentIndex: controller.selectedIndex.value,
+            onTap: controller.onItemTapped,
+            selectedItemColor: Colors.blueGrey,
+            unselectedItemColor: const Color(0xFF526400),
+            showSelectedLabels: false,
+            items: const [
+              BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+              BottomNavigationBarItem(icon: Icon(Icons.search), label: "Search"),
+              BottomNavigationBarItem(icon: Icon(Icons.airplane_ticket), label: "Tickets"),
+              BottomNavigationBarItem(icon: Icon(Icons.person), label: "Profile"),
+            ]),
+      );
+    });
   }
 }
